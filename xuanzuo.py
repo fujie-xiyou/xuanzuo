74bf9dd40f170cadf4cb5ef56dc9be8c86483192
fujie <fujie@xiyoulinux.org>
2019-11-05 17:55:41 +0800

    已经可以了，很多职业选手也没有这么快

--- xuanzuo.py
+++ xuanzuo.py
@@ -5,16 +5,27 @@ from main import XuanZuo
 from main import time_print
 from main import get_time
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
     xuanzuo = XuanZuo()
-    time_print("等待57秒")
-    time.sleep(57)
+    time_print("等待56秒")
+    time.sleep(56)
     while True:
         time_print("准备执行")
         timestamp = int(round(time.time() * 1000))
         xuanzuo.client.get("http://wechat.v2.traceint.com/index.php/prereserve/index.html")
         # print client.page_source
+        if len(xuanzuo.client.find_elements_by_xpath("//h2[contains(text(),'不在预约时间内')]")) > 0:
+            time_print("不在预约时间内 " + str(int(round(time.time() * 1000)) - timestamp) + "ms")
+            print "---------------------------------------"
+            continue
+        if len(xuanzuo.client.find_elements_by_xpath("//h2[contains(text(),'你已经预定了明天')]")) > 0:
+            time_print("已经预定过了！ " + str(int(round(time.time() * 1000)) - timestamp) + "ms")
+            print "---------------------------------------"
+            break
         try:
             title = xuanzuo.client.title
             if not title:
@@ -28,18 +39,17 @@ if __name__ == '__main__':
             site.click()
             time_print("点击了：" + site.text)
             xuanzuo.save_screenshot("点击了座位按钮")
-            tips = xuanzuo.client.find_element_by_id("ti_tips")
+            tips = WebDriverWait(xuanzuo.client, 2, 0.1).until(EC.presence_of_element_located((By.ID, "ti_tips")))
             time_print(tips.text)
+            xuanzuo.save_screenshot(tips.text)
+            if tips.text == "预定座位成功":
+                break
 
         except NoSuchElementException as e:
             print e.msg
-            try:
-                text = xuanzuo.client.find_element_by_xpath("//h2[contains(text(),'不在预约时间内')]")
-                time_print("不在预约时间内 " + str(int(round(time.time() * 1000)) - timestamp) + "ms")
-            except NoSuchElementException as e:
-                time_print("没有座位了！！！")
-                xuanzuo.save_screenshot("没有座位了")
-                break
+            time_print("没有座位了！！！")
+            xuanzuo.save_screenshot("没有座位了")
+            break
         print "---------------------------------------"
 
     xuanzuo.close_clint()
