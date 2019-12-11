81af2cd577795190c870c561a93fbc7fd93a3cd0
fujie <fujie@xiyoulinux.org>
2019-12-12 01:08:56 +0800

    测试截图乱码问题

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+# coding=utf-8
+from main import XuanZuo, time_print
 import time
-while True:
-    print "123"
-    time.sleep(60)
 
+xuanzuo = XuanZuo()
+time_print("准备执行")
+timestamp = int(round(time.time() * 1000))
+xuanzuo.client.get("http://wechat.v2.traceint.com/index.php/prereserve/index.html")
+# print client.page_source
+
+print xuanzuo.client.title
+xuanzuo.save_screenshot("test_charset")
+xuanzuo.close_clint()
